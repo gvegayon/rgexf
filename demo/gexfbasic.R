a8fada6ee2b825e17a026292523922600450172d
Jorge Fabrega <jorge.fabrega@uai.cl>
2012-10-02 23:23:35 -0300

    New demos

---
+++ demo/gexfbasic.R
@@ -0,0 +1,29 @@
+################################################################################
+# Demo of gexf function - basic example 
+################################################################################
+
+pause <- function() {  
+  invisible(readline("\nPress <return> to continue: ")) 
+}
+
+pause()
+## Demos for rgexf - Basic example.
+## The basic function of rgexf requires: 
+## 1. A matrix of nodes 
+## 2. A matrix of edges
+
+# Defining a matrix of nodes
+people <- matrix(c(1:4, 'juan', 'pedro', 'matthew', 'carlos'),ncol=2)
+people
+
+# Defining a matrix of edges
+relations <- matrix(c(1,4,1,2,1,3,2,3,3,4,4,2,2,4,4,1,4,1), ncol=2, byrow=T)
+relations
+
+pause()
+################################################################################
+# Basic network
+
+gexf(nodes=people, edges=relations)
+
+################################################################################
