421626344bca914dd88a91aaec985d0eb8cd5d7c
George Vega <gvega@spensiones.cl>
2012-12-21 14:55:38 -0300

    Improving new reading function

--- R/read.gexf.R
+++ R/read.gexf.R
@@ -3,30 +3,68 @@ read.gexf <- function(x) {
 # Read gexf graph files
 ################################################################################
   # Reads the graph
-  graphfile <- xmlParse(x)
+  gfile <- xmlParse(x)
   
   # Gets the namespace
-  ns <- xmlNamespace(xmlRoot(graphfile))
+  ns <- xmlNamespace(xmlRoot(gfile))
   
   graph <- NULL
-  graph$creator <- xmlValue(
-    getNodeSet(graphfile,"/r:gexf/r:meta/r:creator", c(r=ns))[[1]])
-  graph$description <- xmlValue(
-    getNodeSet(graphfile,"/r:gexf/r:meta/r:description", c(r=ns))[[1]])
-  graph$keywords <- xmlValue(
-    getNodeSet(graphfile,"/r:gexf/r:meta/r:keywords", c(r=ns))[[1]])
+
+  ################################################################################
+  # Creator
+  if (length(x<-getNodeSet(gfile,"/r:gexf/r:meta/r:creator", c(r=ns))) > 0) {
+    graph$creator <- xmlValue(x[[1]])
+  }
+  else graph$creator <- NA
+  # Description
+  if (length(x<-getNodeSet(gfile,"/r:gexf/r:meta/r:description", c(r=ns))) > 0) {
+    graph$description <- xmlValue(x[[1]])
+  }
+  else graph$description <- NA
+  # Keywords
+  if (length(x<-getNodeSet(gfile,"/r:gexf/r:meta/r:keywords", c(r=ns))) > 0) {
+    graph$keywords <- xmlValue(x[[1]])
+  }
+  else graph$keywords <- NA
+  ################################################################################
+
+  # Attributes list
+  if (length(x<-getNodeSet(gfile,"/r:gexf/r:graph/r:attributes", c(r=ns))) > 0) {
+    while (length(x) > 0) {
+      
+      # Gets the class
+      attclass <- paste(xmlAttrs(x[[1]])[["class"]],"att", sep=".")
+      y <- getNodeSet(
+        x[[1]], "/r:gexf/r:graph/r:attributes/r:attribute", c(r=ns))
+      
+      # Builds a dataframe
+      graph[[attclass]] <- data.frame(
+        id=sapply(y, xmlGetAttr, name="id"),
+        title=sapply(y, xmlGetAttr, name="title"),
+        type=sapply(y, xmlGetAttr, name="type")
+        )
+      
+      # Removes the already analyzed
+      x <- x[-1]
+    }
+  }
+  else { # If no other att is
+    graph$nodes.att <- NA
+    graph$edges.att <- NA
+  }
   
-  graph$mode <- xmlAttrs(getNodeSet(graphfile,"/r:gexf/r:graph", c(r=ns))[[1]])
+  
+  graph$mode <- xmlAttrs(getNodeSet(gfile,"/r:gexf/r:graph", c(r=ns))[[1]])
   
   # Nodes
-  nodes <- getNodeSet(graphfile,"/r:gexf/r:graph/r:nodes/r:node", c(r=ns))
+  nodes <- getNodeSet(gfile,"/r:gexf/r:graph/r:nodes/r:node", c(r=ns))
   graph$nodes <- data.frame(
     id=sapply(nodes, xmlGetAttr, name="id"), 
     label=sapply(nodes, xmlGetAttr, name="label"), stringsAsFactors=F)
   rm(nodes)
   
   # Edges
-  edges <- getNodeSet(graphfile,"/r:gexf/r:graph/r:edges/r:edge", c(r=ns))
+  edges <- getNodeSet(gfile,"/r:gexf/r:graph/r:edges/r:edge", c(r=ns))
   graph$edges <- data.frame(
     source=sapply(edges, xmlGetAttr, name="source"), 
     target=sapply(edges, xmlGetAttr, name="target"), stringsAsFactors=F)
@@ -35,4 +73,12 @@ read.gexf <- function(x) {
   graph
 }
 
-# x <- read.gexf("grafo.gexf")
+# Examples
+# ---------
+# x <- read.gexf("http://gephi.org/datasets/airlines-sample.gexf")
+# y <- read.gexf("http://gephi.org/datasets/LesMiserables.gexf")
+# z <- read.gexf("http://diseasome.eu/data/diseasome.gexf")
+# a <- read.gexf("http://noduslabs.com/textplayer/bible.gexf")
+# b <- read.gexf("http://gexf.net/data/WebAtlas_EuroSiS.gexf")
+
+# lapply(b, head)
